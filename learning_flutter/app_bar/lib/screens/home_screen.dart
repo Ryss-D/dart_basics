6aa4a1d2087973f69020b42b19a75a1ef1dd597b
Ryss-D <sebas1999sys@gmail.com>
2020-12-09 18:57:06 -0500

    add the recomended product box and learn about stacks

--- learning_flutter/app_bar/lib/screens/home_screen.dart
+++ learning_flutter/app_bar/lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const PRIMARY = "primary";
 const WHITE = "white";
 const SECONDARY = "secondary";
+const GREY = "grey";
 
 // with this we make agile the color change and avoid erros
 
@@ -10,7 +11,15 @@ const Map<String, Color> myColors = {
   PRIMARY: Color.fromRGBO(226, 115, 138, 1),
   WHITE: Colors.white,
   SECONDARY: Color.fromRGBO(247, 225, 227, 1),
+  GREY: Colors.grey,
 }; // thats a similar way to a dictionary on python, but we have to specify the type of data we will map
+
+var priceTextStyle = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
+
+// we extract the variable as a global variable to reuseit
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -151,7 +160,145 @@ class _Body extends StatelessWidget {
         ),
         Expanded(
           flex: 3,
-          child: Container(),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 12,
+                  top: 12,
+                  left: 40,
+                  right: 25,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Recomended',
+                      style: TextStyle(
+                          color: myColors[WHITE],
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    InkWell(
+                      onTap: () => print("Recommended pressed"),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: myColors[WHITE],
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Icon(
+                            Icons.arrow_forward,
+                            color: myColors[PRIMARY],
+                            size: 20,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: LayoutBuilder(
+                  builder: (context, constraints) => Stack(
+                    children: [
+                      Positioned(
+                        // can give to my items a relative position to stack
+                        right: 10,
+                        child: Container(
+                          height: constraints.maxHeight * 0.8,
+                          width: constraints.maxWidth * 0.8,
+                          decoration: BoxDecoration(
+                            color: myColors[WHITE],
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(70),
+                              topRight: Radius.circular(30),
+                              bottomLeft: Radius.circular(30),
+                              bottomRight: Radius.circular(30),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: 10,
+                        child: Container(
+                          height: constraints.maxHeight * 0.8,
+                          width: constraints.maxWidth * 0.83,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Image(
+                                height: constraints.maxHeight * 0.8,
+                                image: AssetImage('assets/images/makeup4.png'),
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Container(
+                                    width: constraints.maxWidth * 0.4,
+                                    child: Text(
+                                      'Multi shades manu options available',
+                                      style: TextStyle(color: myColors[GREY]),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  Container(
+                                    child: Text(
+                                      '\$75.00',
+                                      style: priceTextStyle,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  )
+                                ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  right: 10,
+                                ),
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    // SizedBox(
+                                    //   height: 10,
+                                    // ),
+                                    FavoriteButton(
+                                      favoriteCallBack: () => print(
+                                        "recomended item favorited",
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    PlusButton(
+                                      addCallBack: () =>
+                                          print("recomended item added"),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     );
@@ -189,14 +336,7 @@ class _ImageTitle extends StatelessWidget {
               padding: const EdgeInsets.only(
                 top: 15,
               ),
-              child: IconButton(
-                onPressed: favoriteCallBack,
-                icon: Icon(
-                  Icons.favorite_border,
-                  color: myColors[PRIMARY],
-                  size: 25,
-                ),
-              ),
+              child: FavoriteButton(favoriteCallBack: favoriteCallBack),
             )
           ],
         ),
@@ -206,42 +346,74 @@ class _ImageTitle extends StatelessWidget {
         Row(
           children: [
             Text(
-              '\$$price',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+              '\$${price.toStringAsFixed(2)}', // like zfill
+              style: priceTextStyle,
             ),
             SizedBox(
               //just a empty box
               width: 20,
             ),
-            InkWell(
-              //wrap any windgets we want into a button
-              onTap: addCallBack,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: myColors[SECONDARY],
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(8),
-                    topRight: Radius.circular(8),
-                    bottomLeft: Radius.circular(8),
-                    bottomRight: Radius.circular(15),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Icon(
-                    Icons.add,
-                    color: myColors[PRIMARY],
-                    size: 15,
-                  ),
-                ),
-              ),
-            ),
+            PlusButton(addCallBack: addCallBack),
           ],
         ),
       ],
+    );
+  }
+}
+
+class PlusButton extends StatelessWidget {
+  const PlusButton({
+    Key key,
+    @required this.addCallBack,
+  }) : super(key: key);
+
+  final Function addCallBack;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      //wrap any windgets we want into a button
+      onTap: addCallBack,
+      child: Container(
+        decoration: BoxDecoration(
+          color: myColors[SECONDARY],
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(8),
+            topRight: Radius.circular(8),
+            bottomLeft: Radius.circular(8),
+            bottomRight: Radius.circular(15),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Icon(
+            Icons.add,
+            color: myColors[PRIMARY],
+            size: 15,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FavoriteButton extends StatelessWidget {
+  const FavoriteButton({
+    Key key,
+    @required this.favoriteCallBack,
+  }) : super(key: key);
+
+  final Function favoriteCallBack;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: favoriteCallBack,
+      icon: Icon(
+        Icons.favorite_border,
+        color: myColors[PRIMARY],
+        size: 25,
+      ),
     );
   }
 }
