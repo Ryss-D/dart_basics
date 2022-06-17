7d51c3b05926498ef1cd85945eed74cd4bdc2019
Ryss-D <ryss-d@protonmail.com>
2022-06-17 15:19:12 -0500

    feat: added appbar buttoon and floating action button

--- personal_expenses_app/lib/main.dart
+++ personal_expenses_app/lib/main.dart
@@ -29,6 +29,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Expenses App"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => {},
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -44,6 +50,11 @@ class MyHomePage extends StatelessWidget {
             UserTransactions(),
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
