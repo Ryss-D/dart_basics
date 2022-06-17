fc689f480b8fa90640c0c009e25e334c147a3450
Ryss-D <ryss-d@protonmail.com>
2022-06-17 14:29:31 -0500

    feat: improved submit fuction logic

--- personal_expenses_app/lib/widgets/new_transaction.dart
+++ personal_expenses_app/lib/widgets/new_transaction.dart
@@ -7,6 +7,16 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount < 0) {
+      return;
+    }
+    addTx(enteredTitle, enteredAmount);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -22,6 +32,7 @@ class NewTransaction extends StatelessWidget {
               //titleInput = value;
               //},
               controller: titleController,
+              onSubmitted: (_) => submitData(),
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
@@ -29,14 +40,13 @@ class NewTransaction extends StatelessWidget {
               //amountInput = value;
               //},
               controller: amountController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => submitData(),
             ),
             TextButton(
               child: Text('Add transaction'),
               //style: color text purple,
-              onPressed: () {
-                addTx(
-                    titleController.text, double.parse(amountController.text));
-              },
+              onPressed: submitData,
             ),
           ],
         ),
