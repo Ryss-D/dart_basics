b72656ae3e49923f7357728e68ec09ecac5c2df2
Sebastian <70545185+Ryss-D@users.noreply.github.com>
2022-06-30 14:03:10 -0500

    feat: added explanatory notes to flutter basics section

--- learning_flutter/layout2/lib/main.dart
+++ learning_flutter/layout2/lib/main.dart
@@ -4,6 +4,7 @@ import 'layout.dart';
 
 void main() => runApp(MyApp());
 
+//random commet to check authory
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
